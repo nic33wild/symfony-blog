32d503356d2ecdd4beb482c14437f1f81f67f166
nic33wild <Nicola-Trentini_student2018@wilder.school>
2019-03-16 02:25:01 +0100

    form bootstrap

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class BlogController extends AbstractController
 {
@@ -39,9 +41,34 @@ class BlogController extends AbstractController
      * @Route("/blog/new", name="blog_create")
      */
     public function create(){
-        return $this->render('blog/create.html.twig');
+        $article = new Article();
+
+        $form = $this->createFormBuilder($article)
+                     ->add('title', TextType::class, [
+                         'attr' => [
+                             'placeholder' => "Title of the article",
+                             'class' => 'form-control'
+                         ]
+                     ])
+                     ->add('content', TextareaType::class, [
+                         'attr' => [
+                             'placeholder' => "Article content",
+                             'class' => 'form-control'
+                         ]
+                     ])
+                     ->add('image', TextType::class, [
+                         'attr' => [
+                             'placeholder' => "Image of the article",
+                             'class' => 'form-control'
+                         ]
+                     ])
+                     ->getForm();
+
+        return $this->render('blog/create.html.twig', [
+            'formArticle' => $form->createView()
+        ]);
     }
-    
+
     /**
      * @Route("/blog/{id}", name="blog_show")
      */
