f6685567597bbae3d845b77badb90f9eea16699f
nic33wild <Nicola-Trentini_student2018@wilder.school>
2019-03-18 00:28:44 +0100

    email unique and created page and route connexion

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,9 +5,14 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @UniqueEntity(
+ *  fields={"email"},
+ *  message="This email is already used !"
+ * )
  */
 class User implements UserInterface
 {
@@ -20,6 +25,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Email()
      */
     private $email;
 
