9d58aab08e46bebb5b35679a1acb7270cf94db5b
nic33wild <Nicola-Trentini_student2018@wilder.school>
2019-03-20 00:50:33 +0100

    removed bootstrap to test

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,6 +12,6 @@ require('@fortawesome/fontawesome-free/css/all.min.css');
 require('@fortawesome/fontawesome-free/js/all.js');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
+const $ = require('jquery');
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
