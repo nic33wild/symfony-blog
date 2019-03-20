9ed9361477c1ff50595e4d966b7e3e2de74b8471
nic33wild <Nicola-Trentini_student2018@wilder.school>
2019-03-20 02:17:47 +0100

    added ck in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+var path = require('path');
 var Encore = require('@symfony/webpack-encore');
 
 Encore
@@ -55,6 +56,16 @@ Encore
     // uncomment if you use API Platform Admin (composer req api-admin)
     //.enableReactPreset()
     //.addEntry('admin', './assets/js/admin.js')
+    .copyFiles([
+        {from: './node_modules/ckeditor/', to: 'ckeditor/[path][name].[ext]', pattern: /\.(js|css)$/, includeSubdirectories: false},
+        {from: './node_modules/ckeditor/adapters', to: 'ckeditor/adapters/[path][name].[ext]'},
+        {from: './node_modules/ckeditor/lang', to: 'ckeditor/lang/[path][name].[ext]'},
+        {from: './node_modules/ckeditor/plugins', to: 'ckeditor/plugins/[path][name].[ext]'},
+        {from: './node_modules/ckeditor/skins', to: 'ckeditor/skins/[path][name].[ext]'}
+    ])
+    .addLoader({test: /\.json$/i, include: [path.resolve(__dirname, 'node_modules/ckeditor')], loader: 'raw-loader', type: 'javascript/auto'})
+;
+
 ;
 
 module.exports = Encore.getWebpackConfig();
